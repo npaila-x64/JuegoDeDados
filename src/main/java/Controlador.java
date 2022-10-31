0db554a35429783f872e7a24a61733b941dd2192
N. Paila <n.paila01@ufromail.cl>
2022-10-31 20:44:34 -0300

    agrega clase controlador y reasigna método main

---
+++ src/main/java/Controlador.java
@@ -0,0 +1,19 @@
+public class Controlador {
+
+    /*
+    Clase controlador
+    Controla el flujo del programa desde un alto nivel de abstracción
+    Su función es disminuir el nivel de acceso de los métodos de la clase Juego
+     */
+
+    private final Juego juego;
+
+    public Controlador() {
+        this.juego = new Juego();
+    }
+
+    public void empezarJuego() {
+        juego.lanzarDados();
+        juego.mostrarGanador();
+    }
+}
